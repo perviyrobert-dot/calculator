81d1ee655899d35e9fd20c4804acfa74e10465a4
perviyrobert-dot <perviyrobert@gmail.com>
2026-06-21 23:02:26 +0300

    Update tokenizer.py

--- calculator/tokenizer.py
+++ calculator/tokenizer.py
@@ -2,14 +2,13 @@
 
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import List
+from typing import List, Union
 
 from .exceptions import TokenizeError
 
 
 class TokenType(Enum):
     """Типы токенов."""
-
     NUMBER = auto()
     IDENTIFIER = auto()
     OPERATOR = auto()
@@ -25,9 +24,8 @@ class TokenType(Enum):
 @dataclass
 class Token:
     """Токен с позицией в строке."""
-
     type: TokenType
-    value: str | float
+    value: Union[str, float]
     position: int
 
 
@@ -53,50 +51,50 @@ class Lexer:
                 continue
 
             # Числа (включая научную нотацию)
-            if ch.isdigit() or (ch == "." and self._is_number_start()):
+            if ch.isdigit() or (ch == '.' and self._is_number_start()):
                 self._tokenize_number()
                 continue
 
             # Идентификаторы
-            if ch.isalpha() or ch == "_":
+            if ch.isalpha() or ch == '_':
                 self._tokenize_identifier()
                 continue
 
             # Операторы и специальные символы
-            if ch == "=":
-                self.tokens.append(Token(TokenType.ASSIGN, "=", self.pos))
+            if ch == '=':
+                self.tokens.append(Token(TokenType.ASSIGN, '=', self.pos))
                 self.pos += 1
                 continue
 
-            if ch == ";":
-                self.tokens.append(Token(TokenType.SEMICOLON, ";", self.pos))
+            if ch == ';':
+                self.tokens.append(Token(TokenType.SEMICOLON, ';', self.pos))
                 self.pos += 1
                 continue
 
-            if ch == ",":
-                self.tokens.append(Token(TokenType.COMMA, ",", self.pos))
+            if ch == ',':
+                self.tokens.append(Token(TokenType.COMMA, ',', self.pos))
                 self.pos += 1
                 continue
 
-            if ch == "(":
-                self.tokens.append(Token(TokenType.LPAREN, "(", self.pos))
+            if ch == '(':
+                self.tokens.append(Token(TokenType.LPAREN, '(', self.pos))
                 self.pos += 1
                 continue
 
-            if ch == ")":
-                self.tokens.append(Token(TokenType.RPAREN, ")", self.pos))
+            if ch == ')':
+                self.tokens.append(Token(TokenType.RPAREN, ')', self.pos))
                 self.pos += 1
                 continue
 
             # Многосимвольные операторы
-            if ch in "*/%+-":
+            if ch in '*/%+-':
                 self._tokenize_operator()
                 continue
 
             # Неизвестный символ
             raise TokenizeError(f"unexpected character '{ch}'", self.pos)
 
-        self.tokens.append(Token(TokenType.EOF, "", self.pos))
+        self.tokens.append(Token(TokenType.EOF, '', self.pos))
         return self.tokens
 
     def _is_number_start(self) -> bool:
@@ -117,27 +115,27 @@ class Lexer:
             if ch.isdigit():
                 value_str += ch
                 self.pos += 1
-            elif ch == "." and not has_dot and not has_exp:
+            elif ch == '.' and not has_dot and not has_exp:
                 has_dot = True
                 value_str += ch
                 self.pos += 1
-            elif ch == "_":
+            elif ch == '_':
                 # Разделитель тысяч (PEP 515)
                 self.pos += 1
                 continue
-            elif ch in "eE" and not has_exp:
+            elif ch in 'eE' and not has_exp:
                 has_exp = True
                 value_str += ch
                 self.pos += 1
                 # Проверяем знак экспоненты
-                if self.pos < self.length and self.expression[self.pos] in "+-":
+                if self.pos < self.length and self.expression[self.pos] in '+-':
                     value_str += self.expression[self.pos]
                     self.pos += 1
             else:
                 break
 
         # Проверка на . без цифр после
-        if value_str == "." or value_str.endswith("."):
+        if value_str == '.' or value_str.endswith('.'):
             raise TokenizeError("invalid number format", start)
 
         try:
@@ -154,7 +152,7 @@ class Lexer:
 
         while self.pos < self.length:
             ch = self.expression[self.pos]
-            if ch.isalnum() or ch == "_":
+            if ch.isalnum() or ch == '_':
                 value += ch
                 self.pos += 1
             else:
@@ -168,41 +166,39 @@ class Lexer:
         ch = self.expression[self.pos]
 
         # Двухсимвольные операторы
-        if ch == "*" and self.pos + 1 < self.length and self.expression[self.pos + 1] == "*":
-            self.tokens.append(Token(TokenType.OPERATOR, "**", start))
+        if ch == '*' and self.pos + 1 < self.length and self.expression[self.pos + 1] == '*':
+            self.tokens.append(Token(TokenType.OPERATOR, '**', start))
             self.pos += 2
             return
 
-        if ch == "/" and self.pos + 1 < self.length and self.expression[self.pos + 1] == "/":
-            self.tokens.append(Token(TokenType.OPERATOR, "//", start))
+        if ch == '/' and self.pos + 1 < self.length and self.expression[self.pos + 1] == '/':
+            self.tokens.append(Token(TokenType.OPERATOR, '//', start))
             self.pos += 2
             return
 
         # Обработка % (бинарный или унарный)
-        if ch == "%":
-            # Если предыдущий токен - число или закрывающая скобка, И следующий токен не число
-            # то это унарный процент (например: 50%, (100+50)%)
+        if ch == '%':
+            # Если предыдущий токен - число или закрывающая скобка
             if self.tokens and self.tokens[-1].type in (TokenType.NUMBER, TokenType.RPAREN):
                 # Проверяем, есть ли после % число (тогда это бинарный оператор)
-                # Например: 17%5 - после % идет число 5
                 next_pos = self.pos + 1
                 while next_pos < self.length and self.expression[next_pos].isspace():
                     next_pos += 1
                 if next_pos < self.length and (
-                    self.expression[next_pos].isdigit() or self.expression[next_pos] == "("
+                    self.expression[next_pos].isdigit() or self.expression[next_pos] == '('
                 ):
                     # Это бинарный оператор %
-                    self.tokens.append(Token(TokenType.OPERATOR, "%", start))
+                    self.tokens.append(Token(TokenType.OPERATOR, '%', start))
                     self.pos += 1
                     return
                 else:
                     # Это унарный процент
-                    self.tokens.append(Token(TokenType.PERCENT_SUFFIX, "%", start))
+                    self.tokens.append(Token(TokenType.PERCENT_SUFFIX, '%', start))
                     self.pos += 1
                     return
             else:
                 # Это бинарный оператор %
-                self.tokens.append(Token(TokenType.OPERATOR, "%", start))
+                self.tokens.append(Token(TokenType.OPERATOR, '%', start))
                 self.pos += 1
                 return
 
